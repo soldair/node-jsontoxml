a37b53817536e090b91f7d453821f89678d1047d
Joscha Feth <joscha@feth.com>
2013-01-22 21:10:20 +0100

    convert to switch

--- jsontoxml.js
+++ jsontoxml.js
@@ -20,61 +20,74 @@ var process_to_xml = function(node_data,options){
     var xml = ""
     , type = typeof node_data
     ;
-
-    //if value is an array create child nodes from values
-    if(node_data instanceof Array){
-      node_data.map(function(v){
-          xml += fn(v,1);
-          //entries that are values of an array are the only ones that can be special node descriptors
-      });
+    if(node_data instanceof Array) {
+      type = 'array';
     } else if(node_data instanceof Date) {
-      // cast dates to ISO 8601 date (soap likes it)
-      xml += node_data.toJSON?node_data.toJSON():node_data+'';
-    } else if (type == 'object'){
+      type = 'date';
+    }
 
-      if(node_descriptor == 1 && node_data.name){
-        var content = ""
-        , attributes = []
-        ;
+    switch(type) {
+    //if value is an array create child nodes from values
+      case 'array':
+        node_data.map(function(v){
+            xml += fn(v,1);
+            //entries that are values of an array are the only ones that can be special node descriptors
+        });
+        break;
 
-        if(node_data.attrs) {
-          if(typeof node_data.attrs != 'object') {
-          // attrs is a string, etc. - just use it as an attribute
-            attributes.push(' ');
-            attributes.push(node_data.attrs);
-          } else {
-            for(var key in node_data.attrs){
-              var value = node_data.attrs[key];
+      case 'date':
+        // cast dates to ISO 8601 date (soap likes it)
+        xml += node_data.toJSON?node_data.toJSON():node_data+'';
+        break;
+
+      case 'object':
+        if(node_descriptor == 1 && node_data.name){
+          var content = ""
+          , attributes = []
+          ;
+
+          if(node_data.attrs) {
+            if(typeof node_data.attrs != 'object') {
+            // attrs is a string, etc. - just use it as an attribute
               attributes.push(' ');
-              attributes.push(key);
-              attributes.push('="')
-              attributes.push(options.escape ? esc(value) : value);
-              attributes.push('"');
+              attributes.push(node_data.attrs);
+            } else {
+              for(var key in node_data.attrs){
+                var value = node_data.attrs[key];
+                attributes.push(' ');
+                attributes.push(key);
+                attributes.push('="')
+                attributes.push(options.escape ? esc(value) : value);
+                attributes.push('"');
+              }
             }
           }
-        }
 
-        //later attributes can be added here
-        if(node_data.text || node_data.value) { 
-          var c = (node_data.value || '')+(node_data.text || '');
-          content += (options.escape?esc(c):c);
-        }
+          //later attributes can be added here
+          if(node_data.text || node_data.value) { 
+            var c = (node_data.value || '')+(node_data.text || '');
+            content += (options.escape?esc(c):c);
+          }
 
-        if(node_data.children){
-          content += fn(node_data.children);
-        }
+          if(node_data.children){
+            content += fn(node_data.children);
+          }
 
-        xml += makeNode(node_data.name, content, attributes.join(''));
+          xml += makeNode(node_data.name, content, attributes.join(''));
 
-      } else {
-        for(var name in node_data){
-          xml += makeNode(name, fn(node_data[name]));
+        } else {
+          for(var name in node_data){
+            xml += makeNode(name, fn(node_data[name]));
+          }
         }
-      }
-    } else if (type == 'function'){
-      xml += node_data(xml,fn);
-    } else {
-      xml += options.escape ? esc(node_data) : node_data;
+        break;
+
+      case 'function':
+        xml += node_data(xml,fn);
+        break;
+
+      default:
+        xml += options.escape ? esc(node_data) : node_data;
     }
 
     return xml;
