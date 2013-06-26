07e036d3a7456cb319d84d9d7f19cebf429e5b0a
Ken <ken.franken@syncbak.com>
2013-06-26 11:33:35 -0500

    add single quote to the set of characters to escape

--- jsontoxml.js
+++ jsontoxml.js
@@ -172,6 +172,7 @@ function esc(str){
   return (''+str).replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
+      .replace(/'/g, '&apos;)
       .replace(/"/g, '&quot;');
 }
 
